31249f3f65793138a450f714a3c68747a0aaf648
Rajan <rajj108@gmail.com>
2017-03-19 18:59:59 -0700

    added popularity

--- models/brands.js
+++ models/brands.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var brandSchema = new Schema({
-  brandName: {
-    type: String,
-    required: [true, 'What is the Brand?']
-  }
+    brandName: {
+        type: String
+        , required: [true, 'What is the Brand?']
+    }
+    , popularity: {
+        type: Number
+    }
 });
-
 var Brand = mongoose.model('Brand', brandSchema);
-
 module.exports = Brand;
